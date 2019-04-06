e3d498ca73e275bf5d106aa3db0a432bb097aae1
SRV <souravdwivedi999@gmail.com>
2019-04-06 20:54:55 +0530

    Create hunter.py

---
+++ hunter.py
@@ -0,0 +1,22 @@
+n=input()
+x=input()
+if x.isalpha() or n.isalpha():
+    print("Invalid")
+else:
+    x=list(x)
+    n=int(n)
+    count=int(0)
+    c=int(0)
+
+    for i in range(0,n):
+        if x[i]!=0:
+            count=0
+            for z in range(i+1,n):
+                if(x[i]==x[z]):
+                    x[z]=0
+                    count=count+1
+            if count!=0:
+                print (int(x[i]),end =" ")
+                c=c+1
+    if c==0:
+        print("unique")
