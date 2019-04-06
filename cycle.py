93163d09f8e835f0582ebacf28cafb54726e516f
SRV <souravdwivedi999@gmail.com>
2019-04-06 09:04:09 +0530

    Update cycle.py

--- cycle.py
+++ cycle.py
@@ -1,7 +1,16 @@
-N= int(input())
-K= int(input())
-arr= input()
-for i in range(K+1):
-    if i == K:
-        new = (arr[-i:] + arr[:-i])
-print(" ".join(new))
+try:
+    n=int(input())
+    k=int(input())
+    array1=[0]*n
+    array2=[0]*n
+
+    for i in range(n):
+        array1[i]=int(input())
+
+    for i in range(n):
+        array2[(i+k)%n]=array1[i]
+
+    for n in array2:
+        print(n,end=' ')
+except:
+    print('invalid')
