5d583b84c211e239e1552eadde0e273806f855ab
SRV <souravdwivedi999@gmail.com>
2019-07-08 18:21:03 +0530

    Update hunter26.py

--- hunter26.py
+++ hunter26.py
@@ -1,7 +1,5 @@
-bb=int(input())
-cc= [int(x) for x in input().split()]
-cc=cc[::-1]
-for i in range(0,bb):
-    print(cc[i],end='')
-    if(i!=bb-1):
-        print("->",end='')
+hh=int(input())
+gg=input()
+gg=gg.split()
+hh=gg[::-1]
+print(*hh,sep='->')
